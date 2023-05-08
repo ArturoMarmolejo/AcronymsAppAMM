a18e456ba06c511272c6a2141f36dc606bb5694e
ArturoMarmolejo <arthur.m.mart@gmail.com>
2023-05-08 16:46:23 -0400

    Fixed layout issues for displaying data. Created README file

--- app/src/main/java/com/arturomarmolejo/acronymsappam/viewmodel/AcronymsViewModel.kt
+++ app/src/main/java/com/arturomarmolejo/acronymsappam/viewmodel/AcronymsViewModel.kt
@@ -21,7 +21,12 @@ import com.arturomarmolejo.acronymsappam.utils.UIState
 import com.arturomarmolejo.acronymsappam.views.adapter.AcronymsAdapter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import okhttp3.internal.notify
 import okhttp3.internal.notifyAll
@@ -43,17 +48,18 @@ class AcronymsViewModel @Inject constructor(
     private val _longFormList: MutableLiveData<UIState<MutableList<AcromineItem>>> = MutableLiveData(UIState.LOADING)
     val longFormList: LiveData<UIState<MutableList<AcromineItem>>> get() = _longFormList
 
-    var searchViewContent: MutableStateFlow<String> = MutableStateFlow("")
+    var searchViewContent: String = ""
+    private val searchQueryChannel = Channel<String>(Channel.CONFLATED)
 
     fun getOnQueryTextListener(): SearchView.OnQueryTextListener{
         return object : OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 query?.let {
-                    if (it == searchViewContent.value)
+                    if (it == searchViewContent)
                         return true
                     if (it.length > 1) {
-                        getAllAcronyms(query)
-                        searchViewContent.value = it
+//                        getAllAcronyms(query)
+                        searchQueryChannel.trySend(it).isSuccess
                         return true
                     }
                 }
@@ -64,6 +70,18 @@ class AcronymsViewModel @Inject constructor(
             }
         }
     }
+
+    init {
+        viewModelScope.launch(ioDispatcher) {
+            searchQueryChannel.consumeAsFlow()
+                .debounce(500)
+                .distinctUntilChanged()
+                .collect() { query ->
+                    getAllAcronyms(query)
+                }
+        }
+    }
+
     fun getAllAcronyms(sf: String? = null, lf: String? = null) {
         viewModelScope.launch(ioDispatcher) {
             selectedSf = sf
@@ -82,7 +100,6 @@ class AcronymsViewModel @Inject constructor(
 
         }
     }
-
 
 }
 
